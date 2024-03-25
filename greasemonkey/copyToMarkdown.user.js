cdcf74fee9fb1142c500909b222aff3e8e736923
Stephen Smithwick <stephen.smithwick@gmail.com>
2024-03-25 12:58:19 -0700

    Update copyToMarkdown.user.js
    Update version

--- greasemonkey/copyToMarkdown.user.js
+++ greasemonkey/copyToMarkdown.user.js
@@ -3,7 +3,7 @@
 // @match       *://*/*
 // @grant       GM_setClipboard
 // @grant       GM_registerMenuCommand
-// @version     0.1
+// @version     0.2
 // @author      Stephen Smithwick
 // ==/UserScript==
 
