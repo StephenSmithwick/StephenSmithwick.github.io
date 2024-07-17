92e5275570df145bf212b97cada2b93f127721be
Stephen Smithwick <stephen.smithwick@gmail.com>
2024-07-16 23:33:40 -0700

    Look for more youtube shorts classes

--- greasemonkey/m.youtube.noshorts.user.js
+++ greasemonkey/m.youtube.noshorts.user.js
@@ -1,28 +1,34 @@
-    // ==UserScript==
-    // @name         Remove Youtube Shorts
-    // @namespace    http://tampermonkey.net/
-    // @version      0.1
-    // @description  Remove Youtube Shorts
-    // @author       Stephen Smithwick
-    // @match        https://m.youtube.com/*
-    // ==/UserScript==
+// ==UserScript==
+// @name         Remove Youtube Shorts
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Remove Youtube Shorts
+// @author       Stephen Smithwick
+// @match        https://m.youtube.com/*
+// ==/UserScript==
 
-    var removeTimeout;
+var removeTimeout;
+const target_class = [
+    "reel-shelf-content-wrapper",
+    "reel-shelf-responsive-layout"
+]
 
-    function removeShorts() {
-        console.log("Removing Shorts");
-        [...document.querySelectorAll(".reel-shelf-content-wrapper")].forEach(
+function removeShorts() {
+    console.log("Removing Shorts");
+    for (const clazz of target_class) {
+        [...document.querySelectorAll(`.${clazz}`)].forEach(
             (elem) => {elem.remove()}
         )
     }
-    const removeAddedShorts = new MutationObserver((mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (
-                mutation.type === "childList" 
-                && mutation.addedNodes 
-                && mutation.target.className == "reel-shelf-content-wrapper"
-            ) { removeShorts(); }
-        }
-    });
+}
+const removeAddedShorts = new MutationObserver((mutationList, observer) => {
+    for (const mutation of mutationList) {
+        if (
+            mutation.type === "childList" 
+            && mutation.addedNodes 
+            && mutation.target.className.split(" ").some(clazz => target_class.includes(clazz))
+        ) { removeShorts(); }
+    }
+});
 
-    removeAddedShorts.observe(document.getElementById("app"), { childList: true, subtree: true });
+removeAddedShorts.observe(document.getElementById("app"), { childList: true, subtree: true });
