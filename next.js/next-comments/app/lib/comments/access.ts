98bdfe30e024b0fb858dd72a7e1254eb2b771328
Stephen Smithwick <stephen.smithwick@gmail.com>
2025-06-27 14:16:40 -0600

    Adding database seed script

--- next.js/next-comments/app/lib/comments/access.ts
+++ next.js/next-comments/app/lib/comments/access.ts
@@ -5,7 +5,6 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function fetchPostComments(post: string): Promise<Comment[]> {
   try {
-    await setupComment();
     const comments = await sql<CommentRow[]>`SELECT
         id, date, post, text, author, avatar_url
       FROM comment
@@ -29,26 +28,12 @@ export async function createComment({
   text: string;
 }) {
   try {
-    await setupComment();
     await sql`INSERT INTO comment (post, author, avatar_url, text, date )
       VALUES (${post}, ${author.name}, ${author.avatar_url}, ${text}, current_timestamp);`;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to insert comment.");
   }
-}
-
-async function setupComment() {
-  await sql`
-    CREATE TABLE IF NOT EXISTS comment (
-      id SERIAL PRIMARY KEY,
-      date TIMESTAMP NOT NULL,
-      post VARCHAR(255) NOT NULL,
-      text TEXT NOT NULL,
-      author TEXT NOT NULL,
-      avatar_url TEXT
-    );
-  `;
 }
 
 function to_comment(res: CommentRow): Comment {
