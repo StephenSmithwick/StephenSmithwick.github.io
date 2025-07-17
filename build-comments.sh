3077a045a7029ef8e9a4c6b14d7d3d332fb646bd
Stephen Smithwick <stephen.smithwick@gmail.com>
2025-07-17 12:37:30 -0600

    Updating slug and styling as well as the solid-comment and hacky build script

--- build-comments.sh
+++ build-comments.sh
@@ -1,6 +1,24 @@
 #! /bin/bash
 
-(cd js-frameworks/solid-comments && pnpm install && pnpm build)
-cp -r js-frameworks/solid-comments/dist/* comments-app
-
 echo "This is a temporary solution to aid in integrating the solid comments client into the blog.  Until we have a better build approach you will also need to update the references to assets in _includes/comments.html after running this script"
+
+# build and replace
+(cd js-frameworks/solid-comments && pnpm install && pnpm build) \
+&& rm -rf comments-app && mkdir -p comments-app \
+&& cp -r js-frameworks/solid-comments/dist/* comments-app
+
+
+
+js=$(ls comments-app/assets/*.js)
+css=$(ls comments-app/assets/*.css)
+
+cat << HTML > _includes/comments.html
+<script type="module" crossorigin src="/${js}"></script>
+<link rel="stylesheet" crossorigin href="/${css}">
+<script>window.POST_SLUG="{{ page.slug }}"</script>
+
+<noscript>You need to enable Comments.</noscript>
+
+<div id="comments"></div>
+<a href="{{ site.comments.app_url }}?post={{ page.slug }}">- comments app</a>
+HTML
