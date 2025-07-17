8a0960bee0c83bacf87e8fcac337c3c8994e8d17
Stephen Smithwick <stephen.smithwick@gmail.com>
2025-07-17 12:22:56 -0600

    Updating solid-comments to use correct API_HOST endpoint

--- js-frameworks/solid-comments/src/lib/comments/env.ts
+++ js-frameworks/solid-comments/src/lib/comments/env.ts
@@ -1,4 +1,5 @@
 export default {
   //stub
-  API_HOST: "https://stephen-smithwick-github-io-comment.vercel.app/",
+  API_HOST: "https://stephen-smithwick-github-io-comment.vercel.app",
+  // API_HOST: "http://localhost:3000",
 };
