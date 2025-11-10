ed7c94c046b4fa8740498ac24833df63ce6bcba6
Stephen Smithwick <stephen.smithwick@gmail.com>
2025-11-10 08:51:25 -0700

    Moving rc.bash to playground

--- rc.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-export GOPATH=$(go env GOPATH)
-export PATH="$PATH:$GOPATH/bin"
-
-# alias kubectl="minikube kubectl --"
-eval "$(/opt/homebrew/bin/brew shellenv)"
-eval "$(rbenv init - zsh)"
-. "$HOME/.cargo/env"
